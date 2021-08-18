9c0f91b617bb1fa5154fb0b8edf1f850a5106b37
michael168999 <64138471+michael168999@users.noreply.github.com>
2021-08-18 20:14:01 +0800

    Delete brian2 directory

--- brian2/cp.bat
+++
@@ -1,6 +0,0 @@
-copy *.* C:\xampp\tomcat\webapps\brian2
-copy css\*.* C:\xampp\tomcat\webapps\brian2\css
-copy buy\*.* C:\xampp\tomcat\webapps\brian2\buy
-copy image\*.* C:\xampp\tomcat\webapps\brian2\image
-
-
